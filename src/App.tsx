37a3ca8f09f5dd95e27464108e52a88d82bc3b23
shirisha1712 <b201559@rgukt.ac.in>
2025-10-17 23:04:37 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import WelcomePage from './components/WelcomePage';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <WelcomePage />
+    </div>
+  );
+};
+
+export default App;
